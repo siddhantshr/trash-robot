ed1b9510dc12840f7ff79c889fd96a987f5a7c9e
Siddhant Sharma <31.siddhant.sharma@gmail.com>
2026-04-06 10:01:25 +0530

    feat: add servo and motor 3

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -4,10 +4,14 @@
 // ============================================================
 
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
 // ---------- UART ----------
 #define RX_PIN 16
 #define BAUD_RATE 9600
+
+// ---------- Servo ----------
+#define SERVO_PIN 13
 
 // ---------- L298N Pins ----------
 #define ENA 12
@@ -18,9 +22,15 @@
 #define IN4 25
 #define ENB 33
 
+// ---------- Second L298N Pins ----------
+#define ENC 19
+#define IN5 18
+#define IN6 5
+
 // ---------- PWM ----------
 #define CH_A 0
 #define CH_B 1
+#define CH_C 2
 #define PWM_FREQ 1000
 #define PWM_RES 8
 
@@ -30,14 +40,25 @@ int bufIndex = 0;
 
 // ---------- Control variables ----------
 int leftSpeed, rightSpeed, F1, F2, S1, S2;
+Servo toolServo;
+
+
+// ============================================================
+// Servo helper
+// ============================================================
+
+void setServoFromF2(int f2) {
+    f2 = constrain(f2, -255, 255);
+    int angle = map(f2, -255, 255, 0, 180);
+    toolServo.write(angle);
+}
 
 
 // ============================================================
 // Motor drive
 // ============================================================
 
-void drive(int speed, int IN_A, int IN_B, int pwmChannel)
-{
+void drive(int speed, int IN_A, int IN_B, int pwmChannel) {
     speed = constrain(speed, -255, 255);
 
     if (speed > 0)
@@ -65,8 +86,7 @@ void drive(int speed, int IN_A, int IN_B, int pwmChannel)
 // Parse packet
 // ============================================================
 
-void parsePacket(char *pkt)
-{
+void parsePacket(char *pkt) {
     sscanf(pkt, "<%d,%d,%d,%d,%d,%d>",
            &leftSpeed,
            &rightSpeed,
@@ -81,8 +101,7 @@ void parsePacket(char *pkt)
 // Setup
 // ============================================================
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
     Serial2.begin(BAUD_RATE, SERIAL_8N1, RX_PIN, -1);
 
@@ -90,12 +109,20 @@ void setup()
     pinMode(IN2, OUTPUT);
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
+    pinMode(IN5, OUTPUT);
+    pinMode(IN6, OUTPUT);
 
     ledcSetup(CH_A, PWM_FREQ, PWM_RES);
     ledcSetup(CH_B, PWM_FREQ, PWM_RES);
+    ledcSetup(CH_C, PWM_FREQ, PWM_RES);
 
     ledcAttachPin(ENA, CH_A);
     ledcAttachPin(ENB, CH_B);
+    ledcAttachPin(ENC, CH_C);
+
+    toolServo.setPeriodHertz(50);
+    toolServo.attach(SERVO_PIN, 500, 2400);
+    setServoFromF2(0);
 }
 
 
@@ -103,8 +130,7 @@ void setup()
 // Main loop
 // ============================================================
 
-void loop()
-{
+void loop() {
     while (Serial2.available())
     {
         char c = Serial2.read();
@@ -115,14 +141,14 @@ void loop()
 
             parsePacket(buffer);
 
-            parsePacket(buffer);
-
             // Serial.println("Hi");
             Serial.printf("L:%d R:%d F1:%d F2:%d S1:%d S2:%d\n",
             leftSpeed, rightSpeed, F1, F2, S1, S2);
 
             drive(leftSpeed, IN1, IN2, CH_A);
             drive(rightSpeed, IN3, IN4, CH_B);
+            drive(F1, IN5, IN6, CH_C);
+            setServoFromF2(F2);
 
             bufIndex = 0;
         }
